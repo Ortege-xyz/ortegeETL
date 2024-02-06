a663577ad63eef998120ed2fc25f8df9286782cb
Pedro <pedroantonioppms@gmail.com>
2024-02-06 13:41:32 -0400

    fix: typing

--- stacketl/jobs/export_blocks_job.py
+++ stacketl/jobs/export_blocks_job.py
@@ -1,3 +1,4 @@
+from stacketl.domain.block import StackBlock
 from stacketl.mappers.block_mapper import StackBlockMapper
 from stacketl.mappers.transaction_mapper import StackTransactionMapper
 from stacketl.api.stack_api import StackApi
@@ -61,9 +62,10 @@ class ExportBlocksJob(BaseJob):
             if block:
                 self._export_block(block)
 
-    def _export_block(self, block):
+    def _export_block(self, block: StackBlock):
         if self.export_blocks:
             self.item_exporter.export_item(self.block_mapper.block_to_dict(block))
+
         if self.export_transactions:
             for tx in block.transactions:
                 self.item_exporter.export_item(self.transaction_mapper.transaction_to_dict(tx))
