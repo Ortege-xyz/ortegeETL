17fff5e813d4bbee12b27ff9c7bc0cf3554d3992
Pedro <pedroantonioppms@gmail.com>
2024-03-20 20:21:46 -0400

    feat: added sorobran service

--- sorobanetl/domain/event.py
+++ sorobanetl/domain/event.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass, asdict
 from datetime import datetime
-from stellar_sdk.xdr import ContractEvent, SCVal
-from typing import Any, Dict, List, Optional, Union
+from stellar_sdk.xdr import SCVal
+from typing import Any, Optional
 
 from sorobanetl.convert_xdr import convert_xdr
-from sorobanetl.domain.transaction import SorobanTransaction
 
 @dataclass
 class SorobanEvent:
@@ -12,25 +11,27 @@ class SorobanEvent:
     ledger: int
     ledger_closed_at: int
     contract_id: str
+    tx_hash: str
     id: int
     paging_token: str
-    closed_at: str
     topic: list[Any]
     value: Any
     in_successful_contract_call: bool
 
     @staticmethod
-    def json_dict_to_event(json_dict: dict):
+    def from_dict(json_dict: dict[str, Any]):
         timestamp_obj = datetime.fromisoformat(json_dict["ledgerClosedAt"].rstrip("Z"))
         json_dict["ledger_closed_at"] = int(timestamp_obj.timestamp())
         json_dict["paging_token"] = json_dict["pagingToken"]
         json_dict["contract_id"] = json_dict["contractId"]
         json_dict["in_successful_contract_call"] = json_dict["inSuccessfulContractCall"]
+        json_dict["tx_hash"] = json_dict["txHash"]
 
         del json_dict["ledgerClosedAt"]
         del json_dict["pagingToken"]
         del json_dict["contractId"]
         del json_dict["inSuccessfulContractCall"]
+        del json_dict["txHash"]
 
         try:
             json_dict["value"] = convert_xdr(SCVal.from_xdr(json_dict["value"]["xdr"]))
@@ -46,7 +47,7 @@ class SorobanEvent:
 
         return SorobanEvent(**json_dict)
     
-    def event_to_dict(self):
+    def to_dict(self):
         event_dict = asdict(self)
         event_dict["type"] = "event"
 
---
+++ sorobanetl/service/soroban_service.py
@@ -0,0 +1,42 @@
+from sorobanetl.api.soroban_rpc import SorobanRpc
+from typing import Any, Optional, Union
+
+from sorobanetl.domain.event import SorobanEvent
+
+class SorobanService():
+    def __init__(self, soroban_rpc: SorobanRpc):
+        self.soroban_rpc = soroban_rpc
+
+    def get_latest_ledger(self):
+        ledger_result = self.soroban_rpc.getLatestLedger()
+
+        if ledger_result is None: 
+            return None
+
+        return ledger_result
+
+    def get_events(self, start_ledger: int, filters: list[dict[str, Any]], pagination: Optional[dict[str, Union[str, int, None]]] = None) -> list[SorobanEvent]:
+        param = {
+            'startLedger': start_ledger,
+            'filters': filters,
+            'pagination': pagination,
+        }
+        result = self.soroban_rpc.getEvents(param)
+
+        if result is None:
+            return []
+        import json
+        events = result.get('events', [])
+
+        return [SorobanEvent.from_dict(event) for event in events]
+
+    def get_ledger_entries(self, entries_keys: list[str]) -> list[dict[str, Union[str, int]]]:
+        param = {
+            'keys': entries_keys,
+        }
+        result = self.soroban_rpc.getLedgerEntries(param)
+        if result is None:
+            return []
+
+        entries = result.get('entries', [])
+        return entries
