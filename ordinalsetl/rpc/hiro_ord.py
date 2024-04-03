4b5a6eed77ec1af38e880d428a982129830de631
Pedro <pedroantonioppms@gmail.com>
2024-04-03 10:51:16 -0400

    feat: export multiple inscriptions from multiple blocks case the rpc is from hiro

--- ordinalsetl/rpc/hiro_ord.py
+++ ordinalsetl/rpc/hiro_ord.py
@@ -6,6 +6,8 @@ GET_STATUS = "ordinals/v1/"
 GET_INSCRIPTION_ID = "ordinals/v1/inscriptions/{id}"
 GET_INSCRIPTIONS = "ordinals/v1/inscriptions"
 
+INSCRIPTIONS_LIMIT = 60
+
 class HiroOrdAPI(OrdRpc, ApiRequester):
 
     def __init__(self, api_url: str, api_key: Optional[str] = None, timeout=10):
@@ -46,7 +48,7 @@ class HiroOrdAPI(OrdRpc, ApiRequester):
     def get_inscriptions_by_block(self, block_number: int):
         response = self._make_get_request(
             GET_INSCRIPTIONS,
-            params={'genesis_block': block_number},
+            params={'genesis_block': block_number, 'limit': 60},
             headers=self.headers,
             timeout=self.timeout
         )
@@ -57,3 +59,28 @@ class HiroOrdAPI(OrdRpc, ApiRequester):
                 data['inscriptions'].append(ins['id'])
             return data
         return None
+    
+    def get_inscriptions_by_blocks(self, from_block: int, to_block: int):
+        inscriptions: list[dict] = []
+        offset = 0
+        while True:
+            response = self._make_get_request(
+                GET_INSCRIPTIONS,
+                params={
+                    'from_genesis_block_height': from_block, 
+                    'to_genesis_block_height': to_block, 
+                    'limit': INSCRIPTIONS_LIMIT,
+                    'offset:': offset,
+                },
+                headers=self.headers,
+                timeout=self.timeout
+            )
+            if response.ok :
+                data = response.json()
+                inscriptions.extend(data['results'])
+
+                if len(inscriptions) < data['total']:
+                    offset += INSCRIPTIONS_LIMIT
+                else:
+                    break
+        return inscriptions
--- ordinalsetl/streaming/ord_streamer_adapter.py
+++ ordinalsetl/streaming/ord_streamer_adapter.py
@@ -1,5 +1,5 @@
 import logging
-import json
+from ordinalsetl.rpc.hiro_ord import HiroOrdAPI
 from ordinalsetl.rpc.ord_rpc import OrdRpc
 from ordinalsetl.streaming.ord_json_transformer import OrdJsonTransformer
 from blockchainetl.jobs.exporters.console_item_exporter import ConsoleItemExporter
@@ -25,8 +25,18 @@ class OrdStreamerAdapter:
         return int(self.ord_rpc.get_block_height())
 
     def export_all(self, start_block, end_block):
-        for x in range(start_block, end_block + 1):
-            self._export_block(x)
+        if isinstance(self.ord_rpc, HiroOrdAPI):
+            inscriptions = self.ord_rpc.get_inscriptions_by_blocks(start_block, end_block)
+            for inscription in inscriptions:
+                inscription['inscription_id'] = inscription.pop('id')
+                inscription['genesis_height'] = inscription.pop('genesis_block_height')
+                inscription['genesis_hash'] = inscription.pop('genesis_block_hash')
+                inscription['output_value'] = inscription.pop('value')
+                formatted = self.transformer.format_inscription(inscription)
+                self.item_exporter.export_item(formatted)
+        else:
+            for x in range(start_block, end_block + 1):
+                self._export_block(x)
 
     def _export_block(self, block):
         logging.info('Exporting block: ' + str(block))
