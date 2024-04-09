9f895306e9ea027423c182fa2e3f52321354d804
Pedro <pedroantonioppms@gmail.com>
2024-04-08 21:13:42 -0400

    fix: block timestamp and first version

--- aptosetl/domain/block.py
+++ aptosetl/domain/block.py
@@ -20,8 +20,8 @@ class AptosBlock:
 
         filtered_data["hash"] = json_dict['block_hash']
         filtered_data["number"] = int(json_dict['block_height'])
-        filtered_data["timestamp"] = int(json_dict['max_gas_amount'])
-        filtered_data["first_version"] = int(json_dict['block_timestamp'])
+        filtered_data["timestamp"] = int(json_dict['block_timestamp'])
+        filtered_data["first_version"] = int(json_dict['first_version'])
         filtered_data["last_version"] = int(json_dict['last_version'])
         
         json_dict["transactions"] = []
