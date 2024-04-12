89467c49d2870a5925471f463d8e671fca1837e3
Pedro <pedroantonioppms@gmail.com>
2024-04-12 12:11:44 -0400

    feat: divide the timestamp to get the seconds

--- aptosetl/domain/block.py
+++ aptosetl/domain/block.py
@@ -20,7 +20,7 @@ class AptosBlock:
 
         filtered_data["hash"] = json_dict['block_hash']
         filtered_data["number"] = int(json_dict['block_height'])
-        filtered_data["timestamp"] = int(json_dict['block_timestamp'])
+        filtered_data["timestamp"] = int(json_dict['block_timestamp']) // 1_000_000 # the timestamp in microseconds we need to divide to get the seconds
         filtered_data["first_version"] = int(json_dict['first_version'])
         filtered_data["last_version"] = int(json_dict['last_version'])
         
