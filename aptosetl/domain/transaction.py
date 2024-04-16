fc4a1333e4baac2fa7947db1b753ad010b480bf7
Pedro <pedroantonioppms@gmail.com>
2024-04-16 09:12:25 -0400

    fix: timestamp from microseconds to seconds

--- aptosetl/domain/transaction.py
+++ aptosetl/domain/transaction.py
@@ -129,7 +129,8 @@ class AptosTransaction:
         filtered_data["max_gas_amount"] = int(json_dict.get('max_gas_amount')) if json_dict.get('max_gas_amount') else None
         filtered_data["gas_unit_price"] = int(json_dict.get('gas_unit_price')) if json_dict.get('gas_unit_price') else None
         filtered_data["expiration_timestamp_secs"] = int(json_dict.get('expiration_timestamp_secs')) if json_dict.get('expiration_timestamp_secs') else None
-        filtered_data["timestamp"] = int(json_dict.get('timestamp')) if json_dict.get('timestamp') else None
+        # the timestamp in microseconds we need to divide to get the seconds
+        filtered_data["timestamp"] = int(json_dict.get('timestamp')) / 1_000_000 if json_dict.get('timestamp') else None
 
         filtered_data["sender"] = json_dict.get('sender')
         filtered_data["state_checkpoint_hash"] = json_dict.get('state_checkpoint_hash')
